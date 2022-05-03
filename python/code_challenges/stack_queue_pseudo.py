8dc4f1df4fa343ad4188426ce362591bf76202dc
Ella Svete <ella.svete@gmail.com>
2022-05-02 18:04:52 -0700

    test 1

--- python/code_challenges/stack_queue_pseudo.py
+++ python/code_challenges/stack_queue_pseudo.py
@@ -1,5 +1,20 @@
+from data_structures.linked_list import Node
 from data_structures.stack import Stack
 
 
-class PseudoQueue:
-    pass
+class PseudoQueue():
+    def __init__(self):
+        self.inbox = Stack()
+        self.outbox = Stack()
+
+    def enqueue(self, value):
+        self.inbox.push(value)
+
+
+    def dequeue(self):
+        return self.inbox.pop()
+        # if self.inbox.is_empty:
+        #     while self.inbox.size() > 0:
+        #         self.outbox.push(self.inbox.pop())
+        # return self.outbox.items.pop()
+
